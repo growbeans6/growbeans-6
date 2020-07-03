5a97f30fcde22dbcaa5da426545c8f46ea582279
hyewon <hyewon>
2020-07-03 16:19:41 +0900

    7월 3일 StudyFileController, studyFolderController

--- cunity/src/main/java/com/growbeans/cunity/studyFile/controller/StudyFileController.java
+++ cunity/src/main/java/com/growbeans/cunity/studyFile/controller/StudyFileController.java
@@ -3,20 +3,35 @@ package com.growbeans.cunity.studyFile.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.growbeans.cunity.studyFile.domain.StudyFile;
 import com.growbeans.cunity.studyFile.service.StudyFileService;
 
 @Controller
 public class StudyFileController {
-	
+
 	@Autowired
 	private StudyFileService studyFileService;
-	
-	public String uploadStudyFile(StudyFile studyFile, Model model, String fileName, String filePath) {
+
+	// 스터디 파일 공유 페이지 보기
+	public ModelAndView studyFileShare() {
 		return null;
 	}
-	
-	/*public String downloadStudyFile(StudyFile studyFile, Model model, )*/
-	
+
+	// 스터디 파일 업로드
+	public String uploadStudyFile() {
+		return null;
+	}
+
+	// 스터디 파일 다운로드
+	public String downloadStudyFile() {
+		return null;
+	}
+
+	// 스터디 파일 삭제
+	public String deleteStudyFile() {
+		return null;
+	}
+
 }
--- cunity/src/main/java/com/growbeans/cunity/studyFolder/controller/StudyFolderController.java
+++ cunity/src/main/java/com/growbeans/cunity/studyFolder/controller/StudyFolderController.java
@@ -1,5 +1,21 @@
 package com.growbeans.cunity.studyFolder.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.growbeans.cunity.studyFolder.service.logic.StudyFolderServiceImpl;
+
 public class StudyFolderController {
 
+	@Autowired
+	private StudyFolderServiceImpl studyFolderService;
+
+	// 스터디 폴더 생성
+	public String insertStudyFolder() {
+		return null;
+	}
+
+	// 스터디 폴더 삭제
+	public String deleteStudyFolder() {
+		return null;
+	}
 }
